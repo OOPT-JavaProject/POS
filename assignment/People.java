89ff85c47cbfa769df923137a5caa7b70834c4e5
BenNie <68955649+BenGorrr@users.noreply.github.com>
2020-07-30 23:25:16 +0800

    Add files via upload

---
+++ assignment/People.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment;
+
+/**
+ *
+ * @author Z
+ */
+public class People {
+    private String name;
+    private int age;
+}
