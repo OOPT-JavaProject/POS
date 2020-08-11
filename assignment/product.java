1fae3ae9a361c1f033cf180f0af1f4fdf1f5997e
Blackie0808 <68988458+Blackie0808@users.noreply.github.com>
2020-08-11 13:17:10 +0800

    Add files via upload

---
+++ assignment/product.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment;
+
+public class product {
+    private String [] productCode = new String[100];
+    private int quantity=0;
+    private static int orderNo=0;
+    
+    public product(String [] productCode, int quantity){
+    this.productCode=productCode;
+    this.quantity=quantity;
+    orderNo++;
+    }
+    
+    public void setProductCode(String [] productCode){
+            this.productCode=productCode;
+    }
+    
+    public String getProductCode(int i){
+        return productCode[i];
+    }
+    
+    public int getQuantity(){
+        return quantity;
+    }
+}
+
+
+
