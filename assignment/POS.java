3b7c8faab8fbe68ec71222c4b3aec64019013297
kai-T3 <68955573+kai-T3@users.noreply.github.com>
2020-08-11 13:46:38 +0800

    Update POS.java

--- assignment/POS.java
+++ assignment/POS.java
@@ -1,26 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package assignment;
 import java.util.ArrayList;
-
+import java.util.Scanner;
+import java.lang.*;
 /**
  *
- * @author Z
+ * @author Kai Yuet
  */
 public class POS {
+    
     public static void main(String[] args) {
+        char staffOrManager;
         Employee.addEmployee("Ben", "M02482", "76541", 2002, 18);
         Employee.addEmployee("Alex", "S02482", "HeckYea", 2011, 21);
         
-        if ("22aw" == "22aw") System.out.println("True");
+        
         for (Employee e: Employee.getEmpList()) { //Print empList
             System.out.println(e);
         }
-    }
-    public static void login() {
+        staffOrManager=login();
+        if(staffOrManager=='M')
+            System.out.println("Login successfully, you are a manager.");
+        else
+            System.out.println("Login successfully, you are a staff.");
         
     }
+    public static char login() {
+        String loginID;
+        String loginPass;
+        char cont;
+        ArrayList<Employee> existingEmpList=Employee.getEmpList();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome, please start by entering your ID and password.");
+        do{
+        System.out.print("Staff ID: ");
+        loginID=scan.nextLine();
+        for(int i=0;i<existingEmpList.size();i++){
+            if(loginID.equals(existingEmpList.get(i).getEmpID())){
+                System.out.print("Staff password: ");
+                loginPass=scan.nextLine();
+                if(loginPass.equals(existingEmpList.get(i).getEmpPassword())){
+                    if(existingEmpList.get(i).getEmpPos().equals("Manager"))
+                        return 'M';
+                    else
+                        return 'S';
+                   
+                }
+            }                         
+        }
+        System.out.print("ID entered doesn't exist.\nContinue to enter another ID?: ");
+        cont=scan.next().charAt(0);
+        scan.nextLine();
+        }while(Character.toUpperCase(cont)!='N');
+        return cont;
+    }
 }
+
